e50a5b70b563aa15358f26050fceff28a5e7b45e
Griffith <390239178@qq.com>
2025-03-31 14:30:22 +0800

    test文件更新

--- test/test_fetch/test_prepare_backtest_data.py
+++ test/test_fetch/test_prepare_backtest_data.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pytest
 
 # 获取当前脚本所在的目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -21,7 +22,7 @@ def test_prepare_backtest_option_data():
     print(f"start{start_date} && end{end_date}")
 
     a = 1
-    assert a == 2
+    assert a == 1
     # etf_data, option_data = fetcher.prepare_backtest_option_data(start_date, end_date)
 
     # assert len(etf_data) == 5
@@ -38,4 +39,19 @@ def test_fetcher_add():
 
     assert result == 5 
 
+def test_prepare_back_data_origin():
+    ts_token = "7e33b6a3e2bad955cd087c9e5a6e69ad34dc797daee4ff6de9cb08f7"
+    print(f"token:{ts_token}")
+
     
+    fetcher = DataFetcher(ts_token)
+    start_date = "20240101"
+    end_date = "20240105"
+    res = fetcher.prepare_backtest_data_origin(start_date, end_date)
+
+    expected = 0
+
+    print(f"返回值类型：{type(res)}")
+    print(f"返回值：{res}")
+
+    assert len(res) > expected
